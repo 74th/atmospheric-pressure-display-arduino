b00775fccd4d0c34365644127d6c99dd92206954
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2025-01-25 06:19:38 +0000

    DHT11

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include <DHT.h>
+#include <Adafruit_NeoPixel.h>
 
 #define SHT30_I2C_ADDR 0x44
 #define SEG7_I2C_ADDR 0x73
 
 #define I2C1_SCL_PIN GPIO_NUM_0
 #define I2C1_SDA_PIN GPIO_NUM_1
+#define DHT11_PIN GPIO_NUM_4
+#define LED_PIN GPIO_NUM_2
+
+DHT dht11 = DHT(GPIO_NUM_4, DHT11);
+Adafruit_NeoPixel pixels(1, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void sht31_softreset()
 {
@@ -24,18 +31,7 @@ void sht31_softreset()
   Wire.endTransmission();
 }
 
-void setup()
-{
-  Wire.begin(I2C1_SDA_PIN, I2C1_SCL_PIN);
-
-  delay(300);
-
-  sht31_softreset();
-
-  delay(300);
-}
-
-void loop()
+void get_sht31()
 {
   printf("GET sht31\r\n");
 
@@ -50,6 +46,8 @@ void loop()
     Wire.write(0x24);
     Wire.write(0x00);
     Wire.endTransmission();
+
+    delay(100);
 
     Wire.requestFrom(SHT30_I2C_ADDR, 6);
     for (int i = 0; i < 6; i++)
@@ -119,7 +117,69 @@ void loop()
   Wire.beginTransmission(SEG7_I2C_ADDR);
   Wire.write(msg, 4);
   Wire.endTransmission();
+}
 
-  delay(10000);
+void init_dht11()
+{
+  dht11.begin();
+}
+
+void get_dht11()
+{
+  float_t temp = dht11.readTemperature();
+  float_t hum = dht11.readHumidity();
+
+  printf("temperature: %f, humidity: %f\r\n", temp, hum);
+  int_fast32_t intHum = hum;
+
+  printf("send hun: %d\r\n", intHum);
+
+  uint8_t msg[4] = {0};
+  msg[0] = 0x10;           // 整数入力用のレジスタアドレス
+  msg[1] = intHum >> 8;    // 上位8bit
+  msg[2] = intHum % 0x100; // 下位8bit
+  msg[3] = 0;              // 小数点の位置
+
+  Wire.beginTransmission(SEG7_I2C_ADDR);
+  Wire.write(msg, 4);
+  Wire.endTransmission();
+}
+
+void setup()
+{
+  pixels.begin();
+  pixels.setPixelColor(0, Adafruit_NeoPixel::Color(0, 32, 0));
+  pixels.show();
+
+  Wire.begin(I2C1_SDA_PIN, I2C1_SCL_PIN);
+
+  delay(300);
+
+#ifdef USE_SHT31
+  sht31_softreset();
+#endif
+
+#ifdef USE_DHT11
+  init_dht11();
+#endif
+
+  delay(300);
+
+  pixels.setPixelColor(0, Adafruit_NeoPixel::Color(0, 0, 32));
+  pixels.show();
+}
+
+void loop()
+{
+#ifdef USE_SHT31
+  get_sht31();
+#endif
+
+#ifdef USE_DHT11
+  get_dht11();
+#endif
+
+  // delay(10000);
   // delay(5000);
+  delay(1000);
 }
